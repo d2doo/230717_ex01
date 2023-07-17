289fd8e61d8853a2bc331692920fe7aee0ac312f
d2doo <hzxvl@naver.com>
2023-07-17 17:45:49 +0900

    아무거나

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,40 @@
 import streamlit as st
 
 
-msg = "Hello World!"
+msg = "Hello World!!!"
 st.write(msg) # 문자열 직접 넣어도 돼요. (streamlit -> print?)
 
 # 터미널(git bash, cmd) : pip install streamlit
+
+import math
+st.write(math.pi)
+
+PI = math.pi
+# 반지름
+radius = 15
+
+PI_is = '원주율 : '
+radius_is = '반지름 : '
+circum = '원의 둘레 : '
+area = '원의 넓이 : '
+
+st.write(PI_is, PI)
+st.write(radius_is, radius)
+st.write(circum, radius * 2 * PI)
+# st.write(area, radius * radius * PI)
+st.write(area, radius ** 2 * PI)
+
+with st.echo():
+    st.write(PI_is, PI)
+    st.write(radius_is, radius)
+    st.write(circum, radius * 2 * PI)
+    # st.write(area, radius * radius * PI)
+    st.write(area, radius ** 2 * PI)
+
+with st.echo():
+    radius = st.number_input("반지름 입력")
+    st.write(PI_is, PI)
+    st.write(radius_is, radius)
+    st.write(circum, radius * 2 * PI)
+    # st.write(area, radius * radius * PI)
+    st.write(area, radius ** 2 * PI)
